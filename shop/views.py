8487029b59ba406f7387e1b56e7c186795d3804a
Dima Gorobets <dimkaua91@gmail.com>
2023-06-30 19:51:02 +0300

    added 3 courses

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
+from .models import Course
 
 
 def index(request):
-    return HttpResponse("Hello from the shop app")
+    courses = Course.objects.all()
+    return HttpResponse(courses)
