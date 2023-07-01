38138150e23b42b40cb319ed0ee68ad2170569d2
Dima Gorobets <dimkaua91@gmail.com>
2023-07-01 16:15:19 +0300

    added routing

--- shop/views.py
+++ shop/views.py
@@ -5,16 +5,16 @@ from .models import Course
 
 def index(request):
     courses = Course.objects.all()
-    return render(request, 'courses.html', {'courses': courses})
+    return render(request, 'shop/courses.html', {'courses': courses})
 
 
 def single_course(request, course_id):
     #Option1 error checking
     # try:
     #     course = Course.objects.get(pk=course_id)
-    #     return render(request, 'single_course.html', {'course': course})
+    #     return render(request, 'shop/single_course.html', {'course': course})
     # except Course.DoesNotExist:
     #     raise Http404()
     #Option2 error checking
     course = get_object_or_404(Course, pk=course_id)
-    return render(request, 'single_course.html', {'course': course})
+    return render(request, 'shop/single_course.html', {'course': course})
