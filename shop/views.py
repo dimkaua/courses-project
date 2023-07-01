8af2d865df3e66473c69545fcb0baad2195eb3ce
Dima Gorobets <dimkaua91@gmail.com>
2023-07-01 15:00:00 +0300

    error checking for page

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Course
 
 
 def index(request):
     courses = Course.objects.all()
     return render(request, 'courses.html', {'courses': courses})
+
+
+def single_course(request, course_id):
+    #Option1 error checking
+    # try:
+    #     course = Course.objects.get(pk=course_id)
+    #     return render(request, 'single_course.html', {'course': course})
+    # except Course.DoesNotExist:
+    #     raise Http404()
+    #Option2 error checking
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'single_course.html', {'course': course})
